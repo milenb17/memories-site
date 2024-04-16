4e0cec02529a2a62b3b6f5912489dbb3a10d05c6
milenb17 <milenb17@gmail.com>
2024-04-16 14:13:05 -0400

    COnnect client to server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,36 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({baseURL: 'http://localhost:8080'})
+const API = axios.create({
+  baseURL: "https://memories-site.onrender.com:8001",
+});
 
 // for each request, if a user is signed in, add their token to req.headers so the backend can access it
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem('profile')) {
-    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
   }
   return req;
-})
+});
 export const fetchPosts = () => {
-  return API.get('/posts');
-}
+  return API.get("/posts");
+};
 
 export const createPost = (newPost) => {
-  return API.post('/posts', newPost);
-}
+  return API.post("/posts", newPost);
+};
 export const updatePost = (currentId, updatedPost) => {
   return API.patch(`/posts/${currentId}`, updatedPost);
-}
+};
 
 export const deletePost = (id) => {
   return API.delete(`/posts/${id}`);
-}
+};
 
 export const likePost = (currentId) => {
   return API.patch(`/posts/${currentId}/like`);
-}
+};
 
 export const logIn = (formData) => {
   return API.post(`/users/login`, formData);
-}
+};
 export const signUp = (formData) => {
   return API.post(`/users/signup`, formData);
-}
+};
